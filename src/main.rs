e170b5f74fc0272355c46c523bd90a153dd1efe4
poly000 <1348292515a@gmail.com>
2024-09-23 02:00:46 +0800

    feat: download images after search

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use lolicon_api::Request;
-use lolicon_api::Category;
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+use std::str::FromStr;
+use std::thread::sleep;
+use std::time::Duration;
+
 use reqwest::blocking::get;
 use serde_json::Value;
 
+use lolicon_api::Category;
+use lolicon_api::Request;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let req = Request::default()
-        // .tag(&["泳装".into()])?
-        .category(Category::Mixin)
-        .proxy("i.pixiv.re");
+        .num(1)?
+        .exclude_ai(true)
+        .category(Category::R18)
+        .aspect_ratio("lt1")?
+        .proxy("i.pixiv.cat");
 
     let url = String::from(req);
 
@@ -23,7 +33,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     if let Some(Value::String(ref image_url)) = original {
-        println!("{image_url}");
+        fs::create_dir_all("images")?;
+
+        let url = url::Url::from_str(&image_url)?;
+        let basename = url.path_segments().unwrap().last().unwrap();
+        let target_path = Path::new("images").join(basename);
+
+        if target_path.exists() {
+            println!("skipping existing image.");
+            return Ok(());
+        }
+        println!(
+            "downloading {image_url} to {}...",
+            target_path.to_string_lossy()
+        );
+
+        let mut image = Err("download failed. exceeding retry limit");
+        for _ in 0..10 {
+            let result = get(url.as_str()).and_then(|result| result.bytes());
+            if let Ok(bytes) = result {
+                if bytes.is_ascii() {
+                    Err("Image not found! may removed by its author")?
+                }
+                image = Ok(bytes);
+            }
+            sleep(Duration::from_millis(500));
+        }
+        let image = image?;
+
+        fs::OpenOptions::new()
+            .write(true)
+            .read(false)
+            .append(false)
+            .create(true)
+            .open(target_path)?
+            .write(&image)?;
     }
 
     Ok(())
