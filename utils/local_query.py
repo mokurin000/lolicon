63abe4af81ba28a39296253c8e0639bbf9552ba9
poly000 <1348292515a@gmail.com>
2024-09-23 19:19:41 +0800

    feat: add local query script (tags)

---
+++ utils/local_query.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import json
+
+
+def read_file(path: str):
+    with open(path, "r", encoding="utf-8") as f:
+        c = json.load(f)
+    return c
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage:", sys.argv[0], "<dir> <tag_group1>..")
+        exit(1)
+
+    directory = sys.argv[1]
+    tag_groups = sys.argv[2:]
+    files = os.listdir(directory)
+
+    metadata_list = (
+        os.path.join(directory, file) for file in files if file.endswith(".json")
+    )
+    print("reading metadata...")
+    metadata = map(read_file, metadata_list)
+    for meta in metadata:
+        for tag_group in tag_groups:
+            if not any(
+                map(
+                    lambda tag: tag in meta["tags"],
+                    map(lambda t: t.strip(), tag_group.split("|")),
+                ),
+            ):
+                break
+        else:
+            print(
+                *(
+                    os.path.realpath(os.path.join(directory, file))
+                    for file in files
+                    if file.startswith(str(meta["pid"])) and not file.endswith(".json")
+                )
+            )
+
+
+if __name__ == "__main__":
+    main()
